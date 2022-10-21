b8e17803c0703341b326e55bdb71c28c9799bbe5
r3kaushi <r3kaushi@uwaterloo.ca>
2022-10-20 21:36:56 -0400

    remove unused vars

--- src/components/heroSection/HeroElements.js
+++ src/components/heroSection/HeroElements.js
@@ -24,26 +24,6 @@ export const HeroContainer = styled.div`
     }
 `
 
-export const HeroBg = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left:  0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-`
-
-export const VideoBg = styled.video`
-    width: 100%;
-    height: 100%;
-    --o-object-fit: cover;
-    object-fit: cover;
-    background: #232a34;
-    opacity: 0.5;
-`
-
 export const HeroContent = styled.div`
     z-index: 3;
     max-width: 1200px;
